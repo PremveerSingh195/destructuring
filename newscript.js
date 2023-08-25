e775ad79b0896838846fb974b5e828c51ce7d90c
PremveerSingh195 <prem.veer2836@gmail.com>
2023-08-25 14:07:55 +0530

    word count generator from string

--- newscript.js
+++ newscript.js
@@ -1,3 +1,20 @@
-function wordcountgenerator(params) {
+function wordcountgenerator(text) {
+    const words = text.toLowerCase().split(" ");
     
+    const wordCountMap = new Map()
+    
+
+    words.forEach(word  => {
+        const count = wordCountMap.get(word) || 0 ;
+        wordCountMap.set(word , count + 1)
+    });
+
+    return wordCountMap;
+   
 }
+
+ const str = "This is a sample text . It is a sample text to demonstrate word count in a text ."
+ const wordCountMap1 = wordcountgenerator(str)
+ console.log(wordCountMap1);
+
+ 
